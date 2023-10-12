828827046151706fcc498b9bf0171a06e3dea9d8
madiha-waqar <mail2madihawaqar@gmail.com>
2023-10-12 13:22:47 -0400

    Create vars for Product/Category counts for admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_AUTHENTICATION_USERNAME'], password: ENV['HTTP_AUTHENTICATION_PASSWORD']
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
