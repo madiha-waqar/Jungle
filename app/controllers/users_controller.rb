102c9ed7b9390a035d0c8b91ce561b9ebf24bf08
madiha-waqar <mail2madihawaqar@gmail.com>
2023-10-12 22:38:22 -0400

    Create new and create actions for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  # Create 'new' action for UsersController
+  def new
+  end
+
+  # Create 'create' action for UsersController
+  def create  
+  end
+  
 end
