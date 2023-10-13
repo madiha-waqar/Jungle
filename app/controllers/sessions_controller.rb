1131fb978f6cd7c0baa4e11dc6d5769eca17b4fc
madiha-waqar <mail2madihawaqar@gmail.com>
2023-10-12 23:09:27 -0400

    Build create action for session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,18 @@ class SessionsController < ApplicationController
 
   # Create 'create' action for SessionsController
   def create
+    @user = User.find_by_email(params[:email])
+
+    # If user exists and password entered is correct
+    if @user && @user.authenticate(params[:password])
+
+      # Save user id as cookie so they stay logged in
+      session[:user_id] = @user.id
+      redirect_to :root
+
+    else 
+      render :new
+    end
   end
 
   # Create 'destroy' action for SessionsController
