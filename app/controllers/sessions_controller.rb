1b18e6893ebc74242d43d9cf87e11e3457d70190
madiha-waqar <mail2madihawaqar@gmail.com>
2023-10-12 23:03:37 -0400

    Create session controller and define its actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  
+  
+  # Create 'new' action for SessionsController
+  def new
+  end
+
+  # Create 'create' action for SessionsController
+  def create
+  end
+
+  # Create 'destroy' action for SessionsController
+  def destroy
+  end
+end
