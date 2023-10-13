77ce27528291d6be03868b25b1c2d967dc709595
madiha-waqar <mail2madihawaqar@gmail.com>
2023-10-12 23:12:40 -0400

    Build destroy action for session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,31 @@
 class SessionsController < ApplicationController
   
   
-  # Create 'new' action for SessionsController
+  # Build 'new' action for SessionsController
   def new
   end
 
-  # Create 'create' action for SessionsController
+  # Build 'create' action for SessionsController
   def create
     @user = User.find_by_email(params[:email])
 
     # If user exists and password entered is correct
     if @user && @user.authenticate(params[:password])
 
-      # Save user id as cookie so they stay logged in
+     # Save the user id inside the browser cookie and it will keep the user 
+    # logged in when he navigate around our website.
       session[:user_id] = @user.id
-      redirect_to :root
-
-    else 
-      render :new
+        redirect_to '/'
+      else
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
     end
   end
 
-  # Create 'destroy' action for SessionsController
+  # Build 'destroy' action for SessionsController
   def destroy
+    # Remove session cookie
+    session[:user_id] = nil
+       redirect_to '/login'
   end
 end
